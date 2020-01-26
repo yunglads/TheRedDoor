308127a8699984cc82aed5e31ee993d58d72c811
AVicuna <aladdinvargas@gmail.com>
2020-01-26 02:02:01 -0800

    Added new events to first room. (Think my work in there is done for now) Started working on next room. Made scripted event where flashlight turns off and light flickers and breaks Tags got messed up when importing fire for fireplace but I managed to fix it. Added audio for breaking floor, picking up note, and light breaking. Fire effect added A LOT of unecessary files. will sift through tomorrow and delete what I dont need.

---
+++ Assets/Scripts/LightBreak.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBreak : MonoBehaviour
+{
+    public Light lightSource;
+    public Light flashlight;
+    AudioSource source;
+    public AudioClip lightBreak;
+    public BoxCollider collider1;
+    public BoxCollider collider2;
+
+    float waitTime;
+    bool startTimer = false;
+    bool playClip = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playClip)
+        {
+            source.PlayOneShot(lightBreak);
+            playClip = false;
+            startTimer = false;
+            Destroy(collider1);
+            Destroy(collider2);
+        }
+
+        if (startTimer)
+        {
+            waitTime += Time.deltaTime;
+        }       
+
+        if (waitTime >= .5f)
+        {
+            lightSource.enabled = false;
+        }
+        if (waitTime >= 1f)
+        {
+            lightSource.enabled = true;
+        }
+        if (waitTime >= 1.5f)
+        {
+            lightSource.enabled = false;
+        }
+        if (waitTime >= 2f)
+        {
+            lightSource.enabled = true;
+        }
+        if(waitTime >= 3f)
+        {
+            playClip = true;
+            lightSource.enabled = false;
+            waitTime = 0f;
+            return;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            startTimer = true;
+            flashlight.enabled = false;
+            //print("Player passed trigger");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    GameObject player;
+    AudioSource source;
+    public AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            source.PlayOneShot(clip);
+        }
+    }
+}
